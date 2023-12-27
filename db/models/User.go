99fd6a064ad47325bf4bd4d5acc42fe86cca28e7
SemyonHoyrish <semyonhoyrish@gmail.com>
2023-12-27 22:20:30 +0500

    Add required fields

--- db/models/User.go
+++ db/models/User.go
@@ -9,10 +9,11 @@ import (
 type User struct {
 	gorm.Model
 
-	ID       uint64 `json:"id"`
-	Name     string `json:"name"`
-	Surname  string `json:"surname"`
-	Nickname string `json:"nickname"`
+	ID           uint64 `json:"id"`
+	Name         string `json:"name"`
+	Surname      string `json:"surname"`
+	Nickname     string `json:"nickname"`
+	PasswordHash uint64 `json:"password_hash"`
 
 	CreatedAt time.Time      `json:"created_at"`
 	EditedAt  time.Time      `json:"edited_at"`
